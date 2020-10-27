e5ea42c74068c2eb252719f3a9f5bf60cdd2921a
bin-dot <72651709+bin-dot@users.noreply.github.com>
2020-10-27 08:39:05 +0700

    Add files via upload

---
+++ tugas ke 4 baru.cpp
@@ -0,0 +1,21 @@
+/* Tugas ke 4 */
+/* sitti aulia sabina r */
+/* 20051397016 */
+
+#include <conio.h>  
+#include <iostream>  
+#include <math.h>  
+
+using namespace std;
+
+int main(){
+int i,b;	
+for(i=1;i<7;i++){
+	 b = pow(i, 3);
+	 
+	 
+	 cout<<b<<",";
+	 	
+	}
+}
+
