dbb7ba3e40cb90798c2d770f732c483f81382736
bin-dot <72651709+bin-dot@users.noreply.github.com>
2020-10-27 08:40:18 +0700

    Add files via upload

---
+++ tabel perkalian.cpp
@@ -0,0 +1,18 @@
+/* membuat tabel perkalian */
+/* sitti aulia sabina r */
+/* 20051397016 */
+
+#include <iostream>
+using namespace std;
+int main ()
+{
+	//tabel perkalian 1-10
+	for (int i=1;i<=10;i++)
+	{
+		for (int j=1; j<=10; j++)
+		{
+			cout << " " << i*j << " \t ";
+		}
+		cout << endl;
+	}
+}
